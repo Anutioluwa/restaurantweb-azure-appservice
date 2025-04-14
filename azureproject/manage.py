d80a992e6ec8a2c4a11a377f4c0f8f65e79d60cf
Anutioluwa <sobowaleodunayoned@gmail.com>
2025-04-14 18:01:39 -0500

    Update manage.py

--- azureproject/manage.py
+++ azureproject/manage.py
@@ -1,6 +1,10 @@
+from dotenv import load_dotenv
+load_dotenv()  # Load the environment variables from .env file
+
+
 SECRET_KEY=ph*#+orfrp2^934%96x7#rn7au#5(c0f#_y01+%(j7gz*&t_@y
 
 DBNAME=restaurantwebserv
 DBHOST=restaurantwebserv.postgres.database.azure.com
-DBUSER=adminuser@restaurantwebserv
+DBUSER=adminuser
 DBPASS=Anuoluwapo1820
