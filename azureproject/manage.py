3cfeb0469ba80147d4918e6857836827ba4e96fe
Anutioluwa <sobowaleodunayoned@gmail.com>
2025-04-14 17:52:54 -0500

    Create manage.py

---
+++ azureproject/manage.py
@@ -0,0 +1,6 @@
+SECRET_KEY=ph*#+orfrp2^934%96x7#rn7au#5(c0f#_y01+%(j7gz*&t_@y
+
+DBNAME=restaurantwebserv
+DBHOST=restaurantwebserv.postgres.database.azure.com
+DBUSER=adminuser@restaurantwebserv
+DBPASS=Anuoluwapo1820
